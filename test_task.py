e15693aabce5392a75377961f47fa8c492a80415
Irina Voron <yarutich.irina@gmail.com>
2024-02-17 22:25:17 +0300

    Updated requirements.txt

---
+++ test_task.py
@@ -0,0 +1,11 @@
+from selene import browser, be, have
+import pytest
+
+@pytest.fixture
+def browser_size():
+    pass
+
+def test_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
