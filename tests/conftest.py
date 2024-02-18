6e2762309d9b9278288a5c134ceb12169d938d81
Irina.Voron <yarutich.irina@gmail.com>
2024-02-18 16:59:54 +0300

    Updated conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from selene import browser
 
-browser.config.base_url = "https://google.com"
-
 
 @pytest.fixture(autouse=True)
 def browser_size():
+    browser.config.base_url = "https://google.com"
     browser.config.window_width = 800
     browser.config.window_height = 800
     yield
